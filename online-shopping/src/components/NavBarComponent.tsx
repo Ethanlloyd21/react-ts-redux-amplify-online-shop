7aa76e1248b888fdcd5c2ddba33ef17208573388
Lloyd's DevMachine <lloydmarcelino@gmail.com>
2023-04-29 09:38:12 -0700

    bootstrap setup

--- online-shopping/src/components/NavBarComponent.tsx
+++ online-shopping/src/components/NavBarComponent.tsx
@@ -1,21 +1,62 @@
 import React from "react";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 const NavBarComponent: React.FC<{}> = ({}) => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect fixed="top" expand="lg" bg="dark" variant="dark">
       <Container>
         <Navbar.Brand href="#home">Shop Online</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Electronics</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <Nav.Link className="nav-link-custom">
+              <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+                Home
+              </Link>
+            </Nav.Link>
+            <Nav.Link className="nav-link-custom">
+              <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+                Electronics
+              </Link>
+            </Nav.Link>
+            <NavDropdown className="nav-link-custom" title="Laptops" id="collasible-nav-dropdown">
+              <NavDropdown.Item>
+                <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+                  Laptops
+                </Link>
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            <NavDropdown
+              className="nav-link-custom"
+              title="Phone"
+              id="collasible-nav-dropdown"
+            >
+              <NavDropdown.Item href="#action/3.1">Phones</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            <NavDropdown className="nav-link-custom" title="TV's" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">TV's</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
               </NavDropdown.Item>
@@ -36,6 +77,6 @@ const NavBarComponent: React.FC<{}> = ({}) => {
       </Container>
     </Navbar>
   );
-}
+};
 
 export default NavBarComponent;
--- online-shopping/src/screens/Navigation.tsx
+++ online-shopping/src/screens/Navigation.tsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'
-import NavBarComponent from '../components/NavBarComponent'
+import React from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import NavBarComponent from "../components/NavBarComponent";
+import Home from "./home/Home";
+import Cart from "./cart/Cart";
 
-
-
-const Navigation: React.FC<{}> =({}) => {
+const Navigation: React.FC<{}> = ({}) => {
   return (
     <>
-        <Router>
-            <NavBarComponent />
-        </Router>
+      <Router>
+        <NavBarComponent />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Router>
     </>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
