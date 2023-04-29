2d9db23ddd6314529a7323fbc21f7608bc94c65a
Lloyd's DevMachine <lloydmarcelino@gmail.com>
2023-04-29 16:12:51 -0700

    starter

--- online-shopping/src/components/NavBarComponent.tsx
+++ online-shopping/src/components/NavBarComponent.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { LinkContainer } from "react-router-bootstrap";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -12,27 +11,32 @@ const NavBarComponent: React.FC<{}> = ({}) => {
   return (
     <Navbar collapseOnSelect fixed="top" expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">Shop Online</Navbar.Brand>
+        <Navbar.Brand>
+          <Link to="/" style={style}>
+            Shop Online
+          </Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <LinkContainer to="/">
-              <Nav.Link className="nav-link-custom">Home</Nav.Link>
-            </LinkContainer>
-            <Nav.Link>
+            <Nav.Link className="nav-link-custom">
               <Link to="/store" style={style}>
                 Store
               </Link>
             </Nav.Link>
-            <NavDropdown title="Laptops" id="collasible-nav-dropdown">
+            <NavDropdown
+              className="nav-link-custom"
+              title="Laptops"
+              id="collasible-nav-dropdown"
+            >
               <NavDropdown.Item>
                 <Link to="/" style={style}>
                   Apple
                 </Link>
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Dell</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">HP</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Lenovo</NavDropdown.Item>
+              <NavDropdown.Item >Dell</NavDropdown.Item>
+              <NavDropdown.Item >HP</NavDropdown.Item>
+              <NavDropdown.Item >Lenovo</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item>
                 <Link to="/laptops" style={style}>
@@ -46,13 +50,13 @@ const NavBarComponent: React.FC<{}> = ({}) => {
               title="Phone"
               id="collasible-nav-dropdown"
             >
-              <NavDropdown.Item href="#action/3.1">Phones</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
+              <NavDropdown.Item >Phones</NavDropdown.Item>
+              <NavDropdown.Item >
                 Another action
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item >Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
+              <NavDropdown.Item >
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
@@ -62,29 +66,26 @@ const NavBarComponent: React.FC<{}> = ({}) => {
               title="TV's"
               id="collasible-nav-dropdown"
             >
-              <NavDropdown.Item href="#action/3.1">TV's</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
+              <NavDropdown.Item >TV's</NavDropdown.Item>
+              <NavDropdown.Item >
                 Another action
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item >Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
+              <NavDropdown.Item >
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
           <Nav>
-            <Nav.Link>
-              <Link
-                to="/cart"
-                style={{ textDecoration: "none", color: "inherit" }}
-              >
+            <Nav.Link className="nav-link-custom">
+              <Link to="/cart" style={style}>
                 View cart
               </Link>
             </Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
+            {/* <Nav.Link eventKey={2} href="#memes">
               Dank memes
-            </Nav.Link>
+            </Nav.Link> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
