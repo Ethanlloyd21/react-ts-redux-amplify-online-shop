b3e98591d93fa804baccb75a7ecb7cd0367220df
Lloyd's DevMachine <lloydmarcelino@gmail.com>
2023-05-05 22:16:42 -0700

    update

--- online-shopping/src/index.tsx
+++ online-shopping/src/index.tsx
@@ -7,9 +7,9 @@ import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
+// import { Amplify } from 'aws-amplify';
+// import awsExports from './aws-exports';
+// Amplify.configure(awsExports);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
