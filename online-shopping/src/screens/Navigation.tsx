d7fc822277bf4c313e496df2aa80c9b6b6b33490
Lloyd's DevMachine <lloydmarcelino@gmail.com>
2023-05-05 22:22:11 -0700

    update

--- online-shopping/src/screens/Navigation.tsx
+++ online-shopping/src/screens/Navigation.tsx
@@ -3,6 +3,11 @@ import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import NavBarComponent from "../components/NavBarComponent";
 import Home from "./home/Home";
 import Cart from "./cart/Cart";
+import Store from "./store/Store";
+import Phones from "./phones/Phones";
+import Laptops from "./laptops/Laptops";
+import Tv from "./tv/Tv";
+import CartContainer from "./cart/CartContainer";
 
 const Navigation = () => {
   return (
@@ -10,8 +15,13 @@ const Navigation = () => {
       <Router>
         <NavBarComponent />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
+          <Route path="/" element={ <Home /> } />
+          <Route path="/cart" element={ <Cart /> } />
+          <Route path="/store" element={ <Store /> } />
+          <Route path="/phones" element={ <Phones /> } />
+          <Route path="/laptops" element={ <Laptops /> } />
+          <Route path="/cartcontainer" element={ <CartContainer /> } />
+          <Route path="/tv" element={ <Tv />} />
         </Routes>
       </Router>
     </>
@@ -19,3 +29,4 @@ const Navigation = () => {
 };
 
 export default Navigation;
+
